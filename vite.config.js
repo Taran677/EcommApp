bb922bc471660213f7f0d2863a3b1446f8c6b5cd
Taranveer Singh <154905330+Taran677@users.noreply.github.com>
2024-06-05 14:52:41 +0530

    added deploying info

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'; // Import defineConfig from Vite
+import react from '@vitejs/plugin-react'; // Import the react plugin
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/EcommApp/",
+  plugins: [react()], // Use the react plugin
+  base: "/EcommApp/dist/",
+  build: {
+    minify: true, // Minify output for production
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove console.* statements
+      },
+    },
+  },
 });
