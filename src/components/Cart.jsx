2255b0f5741e78c732d262db7a5e72292a610335
Taranveer Singh <154905330+Taran677@users.noreply.github.com>
2024-06-15 11:30:24 +0530

    added functionality: empty cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from "react";
-import CardProduct from "./Subcomponents/CardProduct";
 import BuyProduct from "./Subcomponents/BuyProduct";
 import Bill from "./Subcomponents/Bill";
-function Cart({setCartCount}) {
+
+function Cart({ setCartCount }) {
   const [productsInCart, setProductsInCart] = useState([]);
-  setCartCount(productsInCart.length)
+
   useEffect(() => {
     const storedProducts = localStorage.getItem("productsInCart");
     if (storedProducts) {
       setProductsInCart(JSON.parse(storedProducts));
     }
-  }, []);
+    // Update cart count when productsInCart changes
+    setCartCount(productsInCart.length);
+  }, [productsInCart, setCartCount]);
+
+  const emptyCart = () => {
+    setProductsInCart([]);
+    localStorage.setItem("productsInCart", JSON.stringify([]));
+    // Update cart count when cart is emptied
+    setCartCount(0);
+  };
 
   return (
     <div className="container">
       <h2>Shopping Cart</h2>
+      <button className="btn btn-outline-secondary mtc" onClick={emptyCart}>
+        Empty Cart
+      </button>
       <div className="row">
         {productsInCart.map((product, index) => (
           <BuyProduct
@@ -29,10 +41,7 @@ function Cart({setCartCount}) {
           />
         ))}
       </div>
-      <Bill
-        productsInCart={productsInCart}
-        setProductsInCart={setProductsInCart}
-      ></Bill>
+      <Bill productsInCart={productsInCart} setProductsInCart={setProductsInCart} />
     </div>
   );
 }
